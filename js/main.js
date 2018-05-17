5c7e89d1a8929f15807959cfecf11165f184fd23
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-05-17 11:23:47 -0500

    added anothr cat

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-const clickCat =  document.querySelector(".cat");
-let numOfClicks = document.querySelector(".click");
-let clicks = 0;
+const clickCat1 =  document.querySelector(".cat1");
+const clickCat2 = document.querySelector(".cat2")
+let numOfClicks1 = document.querySelector(".click1");
+let numOfClicks2 = document.querySelector(".click2");
+let name1 =  document.querySelector(".cat1Name");
+let name2 = document.querySelector(".cat2Name");
 
-clickCat.addEventListener('click', function increaseClicks() {
-    
-    clicks++;
-    numOfClicks.textContent = clicks;
+name1.textContent = "Missy";
+name2.textContent = "Molly";
+
+clickCat1.addEventListener('click', function increaseClicks() {
+    numOfClicks1.textContent++;
+})
+
+clickCat2.addEventListener('click', function increaseClicks() {
+    numOfClicks2.textContent++;
 })
